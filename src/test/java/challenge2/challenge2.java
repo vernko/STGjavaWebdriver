6aebeb53751e65783b71c98ed676116d6d4fc736
Vern Kofford <cheow3@gmail.com>
2019-09-06 15:22:53 -0600

    Completed challenge 2

---
+++ src/test/java/challenge2/challenge2.java
@@ -0,0 +1,59 @@
+package challenge2;
+
+import com.sun.glass.ui.Window;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class challenge2 {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeSuite
+    public void startSuite() throws Exception {
+    }
+
+    @AfterSuite
+    public void stopSuite() throws Exception {
+        System.out.println("All done!!!");
+    }
+
+    @BeforeClass
+    public void startClass() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "./Binaries/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterClass
+    public void stopClass() {
+        driver.quit();
+    }
+
+    @BeforeMethod()
+    public void beforeMethod() throws Exception {
+    }
+
+    @AfterMethod()
+    public void afterMethod() {
+    }
+
+    @Test()
+    public void testChallengeTwo() throws Exception {
+        driver.get("https://www.copart.com");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-search")));
+
+        driver.findElement(By.id("input-search")).sendKeys("exotics" + Keys.ENTER);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("zip-code")));
+        
+        WebElement porsche = (driver.findElement(By.id("serverSideDataTable")));
+        Assert.assertEquals(porsche.getText().contains("PORSCHE"), true);
+    }
+}
